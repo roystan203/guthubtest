b6c8a79656ec1cbc1c6a177ed45a2b0bfdc026a5
roystan203 <64949108+roystan203@users.noreply.github.com>
2020-08-23 13:10:18 +0530

    add a sample python file

---
+++ tutorial17_functions.py
@@ -0,0 +1,11 @@
+__author__ = "The automation zone"
+# function syntax [ A function is a block of code which runs when it is called. ]
+# passing and returning values, default value
+
+def add(num1, num2):
+    return num1 + num2
+
+a = add(5)
+
+print(a)
+
